d3510ca9d4a1c42d4d90f91bda78bd4d60f36f1a
paperspace <pierson.we@gmail.com>
2018-02-01 14:08:50 -0800

    remove packages

--- packages/keras_rcnn/layers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .losses import (RCNN, RCNNMaskLoss, RPN)
-
-from .object_detection import (AnchorTarget, ObjectProposal, ProposalTarget)
-
-from ._pooling import RegionOfInterest
-
-from ._object_detection import ObjectDetection
-
-from ._upsample import Upsample
