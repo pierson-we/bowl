77d7203497d78a4d4025e14e6be04dac70e896cf
pierson-we <pierson.we@gmail.com>
2018-02-01 14:03:10 -0800

    Update __init__.py

--- packages/keras_rcnn/__init__.py
+++ packages/keras_rcnn/__init__.py
@@ -2,3 +2,9 @@ from __future__ import absolute_import
 
 from . import models
 from . import backend
+from . import classifiers
+from . import data
+from . import datasets
+from . import layers
+from . import metrics
+from . import preprocessing
