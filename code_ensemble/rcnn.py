f68598ead071f3a502419dd11b15d2dab5852ea6
pierson-we <pierson.we@gmail.com>
2018-02-01 13:55:13 -0800

    Update rcnn.py

--- code_ensemble/rcnn.py
+++ code_ensemble/rcnn.py
@@ -1,7 +1,7 @@
 import sys
-sys.path.insert(0, "../packages")
-import cv2
 import os
+sys.path.insert(0, os.path.join(os.pardir, 'packages'))
+import cv2
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
