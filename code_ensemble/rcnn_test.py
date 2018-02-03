80908513da03f346afc0fee10677e2683f92296b
pierson-we <pierson.we@gmail.com>
2018-02-03 00:07:38 -0800

    Update rcnn_test.py

--- code_ensemble/rcnn_test.py
+++ code_ensemble/rcnn_test.py
@@ -19,7 +19,7 @@ training, test = datasets.load_data('DSB2018')
 # training, validation = sklearn.model_selection.train_test_split(training)
 
 classes = {
-    "nucleus": 1
+	"nucleus": 1
 }
 
 # with open(train_json_file, 'r') as file:
@@ -47,7 +47,7 @@ for item in training:
 		item['boxes'].append({})
 		#item['boxes'][-1]['class'] = x['class']
 		item['boxes'][-1] = [x['bounding_box']['minimum']['c'], x['bounding_box']['minimum']['r'], 
-				     x['bounding_box']['maximum']['c'], x['bounding_box']['maximum']['r']]
+					 x['bounding_box']['maximum']['c'], x['bounding_box']['maximum']['r']]
 		#item['boxes'][-1]['x1'] = x['bounding_box']['minimum']['c']
 		#item['boxes'][-1]['x2'] = x['bounding_box']['maximum']['c']
 		#item['boxes'][-1]['y1'] = x['bounding_box']['minimum']['r']
@@ -100,10 +100,10 @@ for _ in range(0,10):
 	axis.imshow(target_image)
 
 	for target_index, target_score in enumerate(target_scores):
-	    if target_score > 0:
+		if target_score > 0:
 		xy = [
-		    target_bounding_boxes[target_index][0],
-		    target_bounding_boxes[target_index][1]
+			target_bounding_boxes[target_index][0],
+			target_bounding_boxes[target_index][1]
 		]
 
 		w = target_bounding_boxes[target_index][2] - target_bounding_boxes[target_index][0]
@@ -184,23 +184,23 @@ _, axis = matplotlib.pyplot.subplots(1)
 axis.imshow(target_image)
 for index, label in enumerate(target_labels):
 	if label == 1:
-	    xy = [
+		xy = [
 		target_bounding_boxes[index][0],
 		target_bounding_boxes[index][1]
-	    ]
-	    w = target_bounding_boxes[index][2] - target_bounding_boxes[index][0]
-	    h = target_bounding_boxes[index][3] - target_bounding_boxes[index][1]
-	    rectangle = matplotlib.patches.Rectangle(xy, w, h, edgecolor="g", facecolor="none")
-	    axis.add_patch(rectangle)
+		]
+		w = target_bounding_boxes[index][2] - target_bounding_boxes[index][0]
+		h = target_bounding_boxes[index][3] - target_bounding_boxes[index][1]
+		rectangle = matplotlib.patches.Rectangle(xy, w, h, edgecolor="g", facecolor="none")
+		axis.add_patch(rectangle)
 for index, score in enumerate(output_scores):
 	if score > 0.95:
-	    xy = [
+		xy = [
 		output_anchors[index][0],
 		output_anchors[index][1]
-	    ]
-	    w = output_anchors[index][2] - output_anchors[index][0]
-	    h = output_anchors[index][3] - output_anchors[index][1]
-	    rectangle = matplotlib.patches.Rectangle(xy, w, h, edgecolor="r", facecolor="none")
-	    axis.add_patch(rectangle)
+		]
+		w = output_anchors[index][2] - output_anchors[index][0]
+		h = output_anchors[index][3] - output_anchors[index][1]
+		rectangle = matplotlib.patches.Rectangle(xy, w, h, edgecolor="r", facecolor="none")
+		axis.add_patch(rectangle)
 matplotlib.pyplot.show()
 
