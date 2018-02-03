d981101559a1fbdbde512ae8aaa9e0a22d4b1210
pierson-we <pierson.we@gmail.com>
2018-02-03 14:01:33 -0800

    Update rcnn_test.py

--- code_ensemble/rcnn_test.py
+++ code_ensemble/rcnn_test.py
@@ -216,41 +216,42 @@ model.fit_generator(generator, epochs=10, steps_per_epoch=len(training))
 model.save_weights(weights_path)
 
 # visualize prediction
-example, _ = next(generator)
-target_bounding_boxes, target_image, target_labels, _ = example
-target_bounding_boxes = numpy.squeeze(target_bounding_boxes)
-target_image = numpy.squeeze(target_image)
-target_labels = numpy.argmax(target_labels, -1)
-target_labels = numpy.squeeze(target_labels)
-# output_anchors, output_proposals, output_deltas, output_scores = model.predict(example)
-output_anchors, output_scores = model.predict(example)
-output_anchors = numpy.squeeze(output_anchors)
-# output_proposals = numpy.squeeze(output_proposals)
-# output_deltas = numpy.squeeze(output_deltas)
-output_scores = numpy.squeeze(output_scores)
-print(output_anchors)
-print(output_scores)
-_, axis = matplotlib.pyplot.subplots(1)
-axis.imshow(target_image)
-for index, label in enumerate(target_labels):
-	if label == 1:
-		xy = [
-		target_bounding_boxes[index][0],
-		target_bounding_boxes[index][1]
-		]
-		w = target_bounding_boxes[index][2] - target_bounding_boxes[index][0]
-		h = target_bounding_boxes[index][3] - target_bounding_boxes[index][1]
-		rectangle = matplotlib.patches.Rectangle(xy, w, h, edgecolor="g", facecolor="none")
-		axis.add_patch(rectangle)
-for index, score in enumerate(output_scores):
-	if score[1] > 0.25:
-		xy = [
-		output_anchors[index][0],
-		output_anchors[index][1]
-		]
-		w = output_anchors[index][2] - output_anchors[index][0]
-		h = output_anchors[index][3] - output_anchors[index][1]
-		rectangle = matplotlib.patches.Rectangle(xy, w, h, edgecolor="r", facecolor="none")
-		axis.add_patch(rectangle)
-matplotlib.pyplot.show()
+for i in range(0, 10):
+	example, _ = next(generator)
+	target_bounding_boxes, target_image, target_labels, _ = example
+	target_bounding_boxes = numpy.squeeze(target_bounding_boxes)
+	target_image = numpy.squeeze(target_image)
+	target_labels = numpy.argmax(target_labels, -1)
+	target_labels = numpy.squeeze(target_labels)
+	# output_anchors, output_proposals, output_deltas, output_scores = model.predict(example)
+	output_anchors, output_scores = model.predict(example)
+	output_anchors = numpy.squeeze(output_anchors)
+	# output_proposals = numpy.squeeze(output_proposals)
+	# output_deltas = numpy.squeeze(output_deltas)
+	output_scores = numpy.squeeze(output_scores)
+	print(output_anchors)
+	print(output_scores)
+	_, axis = matplotlib.pyplot.subplots(1)
+	axis.imshow(target_image)
+	for index, label in enumerate(target_labels):
+		if label == 1:
+			xy = [
+			target_bounding_boxes[index][0],
+			target_bounding_boxes[index][1]
+			]
+			w = target_bounding_boxes[index][2] - target_bounding_boxes[index][0]
+			h = target_bounding_boxes[index][3] - target_bounding_boxes[index][1]
+			rectangle = matplotlib.patches.Rectangle(xy, w, h, edgecolor="g", facecolor="none")
+			axis.add_patch(rectangle)
+	for index, score in enumerate(output_scores):
+		if score[1] > 0.25:
+			xy = [
+			output_anchors[index][0],
+			output_anchors[index][1]
+			]
+			w = output_anchors[index][2] - output_anchors[index][0]
+			h = output_anchors[index][3] - output_anchors[index][1]
+			rectangle = matplotlib.patches.Rectangle(xy, w, h, edgecolor="r", facecolor="none")
+			axis.add_patch(rectangle)
+	matplotlib.pyplot.show()
 
