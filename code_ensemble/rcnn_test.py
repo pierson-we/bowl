af2645d194ffaf55cbd69dc5d17f2e2ff11ee5a6
pierson-we <pierson.we@gmail.com>
2018-02-02 22:53:22 -0800

    Update rcnn_test.py

--- code_ensemble/rcnn_test.py
+++ code_ensemble/rcnn_test.py
@@ -72,6 +72,7 @@ generator = generator.flow(training, classes, target_shape=(img_size, img_size),
 
 
 for x in range(0, 5):
+	print('loading one image')
 	(target_bounding_boxes, target_image, target_scores, _), _ = generator.next()
 	print('loaded one image')
 	target_bounding_boxes = numpy.squeeze(target_bounding_boxes)
